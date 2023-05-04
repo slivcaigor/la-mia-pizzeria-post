39283a118753ccd135310284d76ce37cf8797fd7
slivcaigor <slivcaigor@gmail.com>
2023-05-04 15:02:58 +0200

    Add project files.

---
+++ la-mia-pizzeria-static/Models/Listing.cs
@@ -0,0 +1,12 @@
+﻿namespace la_mia_pizzeria_static.Models
+{
+    public class Listing
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Image { get; set; }
+        public string Description { get; set; }
+        public double Price { get; set; }
+        public bool IsNew { get; set; }
+    }
+}
